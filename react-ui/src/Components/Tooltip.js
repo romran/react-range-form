3c1c6360f6d652f9939456db57c34660956afc93
romran <flowestinfo@gmail.com>
2018-04-12 02:52:00 +0300

    react init

---
+++ react-ui/src/Components/Tooltip.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Tooltip from 'rc-tooltip';
+import PropTypes from 'prop-types';
+import '../Assets/css/bootstrap_white.css';
+
+class Tip extends Component {
+    render() {
+        const { overlay, classTitle } = this.props
+        return (
+            <Tooltip
+                trigger={['hover', 'click']}
+                placement="bottomLeft"
+                overlay={<span>{overlay}</span>}
+                arrowContent={<div className="rc-tooltip-arrow-inner"></div>}
+                align={{
+                    offset: [-20, 3],
+                }}
+            >
+                <a href="tooltip" className={classTitle}></a>
+            </Tooltip>
+        )
+    }
+}
+
+Tip.propTypes = {
+    overlay: PropTypes.string,
+    classTitle: PropTypes.string,
+};
+
+export default Tip;
