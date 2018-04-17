74e4e7ca5ef19b0663ea7c8bc4497b008ec1cfa9
romran <flowestinfo@gmail.com>
2018-04-17 15:44:28 +0300

    clean up

--- react-ui/src/Components/ConditionalTitle.js
+++ react-ui/src/Components/ConditionalTitle.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { getFormState } from './finalFormDuck'
-import PropTypes from 'prop-types';
-
+ 
 const ConditionalTitle = ({ state }) => (
     <h2>
         Informacija apie jūsų {state.values.CreditFor === "Family" ? 'ir bendraskolio' : ''} pajamas {state.values.RequestedLoanAmount >= 4000 ? 'ir turtą' : ''}
